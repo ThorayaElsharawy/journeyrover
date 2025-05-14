78b4cf56d15fb091c04d4cc272da5c448277f930
thoraya elsharawy <thorayaelsharawy@gmail.com>
2025-05-14 13:51:25 +0300

    fix: rmove promise fun

--- src/app/countries/[country]/page.tsx
+++ src/app/countries/[country]/page.tsx
@@ -4,16 +4,9 @@ export const metadata = {
     title: 'country | Journey Rover'
 }
 
-export default async function Page({ params }: { params: { country: string } }) {
+export default function Page({ params }: { params: { country: string } }) {
     const { country } = params
     console.log(country); // Works now
-
-    await new Promise<void>((resolve) => {
-        setTimeout(() => {
-            resolve()
-        }, 2000)
-    })
-
     return (
         <>Hiiii</>
     )
